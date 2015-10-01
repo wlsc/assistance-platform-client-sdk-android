9bd7136aded0b0c2be24d16501ec00c9ea786502
Wladimir Schmidt <wlsc.dev@gmail.com>
2015-10-01 23:57:13 +0200

    * removed setTimestamp from IDbUpdatableSensor.java

--- Kraken.MeSDK/src/de/tudarmstadt/informatik/tk/android/kraken/interfaces/IDbUpdatableSensor.java
+++ Kraken.MeSDK/src/de/tudarmstadt/informatik/tk/android/kraken/interfaces/IDbUpdatableSensor.java
@@ -2,8 +2,6 @@ package de.tudarmstadt.informatik.tk.android.kraken.interfaces;
 
 public interface IDbUpdatableSensor extends IDbSensor {
 
-	public void setTimestamp(Long timestamp);
-
 	public Boolean getIsNew();
 
 	public Boolean getIsUpdated();
