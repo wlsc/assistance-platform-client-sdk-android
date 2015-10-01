145f23b1953ce1a2d73b98021443f2bcaad17e51
Wladimir Schmidt <wlsc.dev@gmail.com>
2015-10-02 00:12:03 +0200

    * fixed bug with not connected Google Api Client on disconnect

--- Kraken.MeSDK/src/de/tudarmstadt/informatik/tk/android/kraken/model/db/sensors/triggered/ActivitySensor.java
+++ Kraken.MeSDK/src/de/tudarmstadt/informatik/tk/android/kraken/model/db/sensors/triggered/ActivitySensor.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentSender.SendIntentException;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -21,9 +22,7 @@ import de.tudarmstadt.informatik.tk.android.kraken.service.ActivitySensorService
 
 public class ActivitySensor extends AbstractTriggeredSensor implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
-    public enum Configuration {
-        DETECTION_INTERVAL_IN_SEC;
-    }
+    private static final String TAG = ActivitySensor.class.getSimpleName();
 
     // ------------------- Configuration -------------------
     private int DETECTION_INTERVAL_IN_SEC = 120;
@@ -72,32 +71,44 @@ public class ActivitySensor extends AbstractTriggeredSensor implements GoogleApi
     public void stopSensor() {
         if (mGoogleApiClient != null) {
             try {
-                ActivityRecognition.ActivityRecognitionApi.removeActivityUpdates(mGoogleApiClient, m_activityRecognitionPendingIntent);
-                mGoogleApiClient.disconnect();
+                if (mGoogleApiClient.isConnected()) {
+                    ActivityRecognition.ActivityRecognitionApi.removeActivityUpdates(mGoogleApiClient, m_activityRecognitionPendingIntent);
+                    mGoogleApiClient.disconnect();
+                }
             } catch (IllegalStateException e) {
-                e.printStackTrace();
+
+                Log.e(TAG, "Cannot disconnect from Google Api!", e);
+
+                // stopping sensor
+                isRunning = false;
             }
         }
+
         isRunning = false;
     }
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
+
+        Log.d(TAG, "Connection failed.");
+
         // Turn off the request flag
         isRunning = false;
+
         /*
          * If the error has a resolution, start a Google Play services activity
 		 * to resolve it.
 		 */
         if (connectionResult.hasResolution()) {
+
             try {
                 connectionResult.startResolutionForResult((Activity) context, CONNECTION_FAILURE_RESOLUTION_REQUEST);
             } catch (SendIntentException e) {
-                // Log the error
-                e.printStackTrace();
+                Log.e(TAG, "Cannot start resolution", e);
             }
             // If no resolution is available, display an error dialog
         } else {
+            Log.d(TAG, "Cannot find any resolution");
             // // Get the error code
             // int errorCode = connectionResult.getErrorCode();
             // // Get the error dialog from Google Play services
@@ -111,8 +122,12 @@ public class ActivitySensor extends AbstractTriggeredSensor implements GoogleApi
 
     @Override
     public void onConnected(Bundle arg0) {
+
+        Log.d(TAG, "Connection established.");
+
         Intent intent = new Intent(context, ActivitySensorService.class);
         m_activityRecognitionPendingIntent = PendingIntent.getService(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
         ActivityRecognition.ActivityRecognitionApi.requestActivityUpdates(mGoogleApiClient, DETECTION_INTERVAL_IN_SEC * 1000, m_activityRecognitionPendingIntent);
         //mGoogleApiClient.disconnect();
     }
