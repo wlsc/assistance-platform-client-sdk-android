676b9ec0ca733cf299394ae158065f74a9e674a9
Wladimir Schmidt <wlsc.dev@gmail.com>
2015-10-08 22:44:52 +0200

    * moved retrofit custom error handler to sdk

---
+++ Kraken.MeSDK/src/de/tudarmstadt/informatik/tk/android/kraken/model/api/exception/BadRequestException.java
@@ -0,0 +1,12 @@
+package de.tudarmstadt.informatik.tk.android.kraken.model.api.exception;
+
+/**
+ * @author Wladimir Schmidt (wlsc.dev@gmail.com)
+ * @date 21.08.2015
+ */
+public class BadRequestException extends Exception {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
---
+++ Kraken.MeSDK/src/de/tudarmstadt/informatik/tk/android/kraken/model/api/exception/ServiceTemporaryNotAvailableException.java
@@ -0,0 +1,11 @@
+package de.tudarmstadt.informatik.tk.android.kraken.model.api.exception;
+
+/**
+ * @author Wladimir Schmidt (wlsc.dev@gmail.com)
+ * @date 21.08.2015
+ */
+public class ServiceTemporaryNotAvailableException extends Exception {
+
+    public ServiceTemporaryNotAvailableException(String message) {
+    }
+}
