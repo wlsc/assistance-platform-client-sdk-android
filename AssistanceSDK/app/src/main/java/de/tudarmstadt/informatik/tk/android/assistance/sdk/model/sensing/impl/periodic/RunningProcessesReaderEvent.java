95bbb70f51440eef1baf6c12ac9b4b789d6364c5
Wladimir Schmidt <wlsc.dev@gmail.com>
2015-11-24 22:12:17 +0100

    * adjusted RunningProcessesReaderEvent to new db schema

--- AssistanceSDK/app/src/main/java/de/tudarmstadt/informatik/tk/android/assistance/sdk/model/sensing/impl/periodic/RunningProcessesReaderEvent.java
+++ AssistanceSDK/app/src/main/java/de/tudarmstadt/informatik/tk/android/assistance/sdk/model/sensing/impl/periodic/RunningProcessesReaderEvent.java
@@ -5,31 +5,45 @@ import android.app.ActivityManager.RunningAppProcessInfo;
 import android.content.Context;
 
 import java.util.ArrayList;
-import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
+import de.tudarmstadt.informatik.tk.android.assistance.sdk.db.DbRunningProcessesEvent;
 import de.tudarmstadt.informatik.tk.android.assistance.sdk.model.api.dto.DtoType;
 import de.tudarmstadt.informatik.tk.android.assistance.sdk.model.sensing.AbstractPeriodicEvent;
 import de.tudarmstadt.informatik.tk.android.assistance.sdk.model.sensing.ISensor;
+import de.tudarmstadt.informatik.tk.android.assistance.sdk.util.DateUtils;
 
-
+/**
+ * @author Unknown
+ * @edited by Wladimir Schmidt (wlsc.dev@gmail.com)
+ * @date 24.11.2015
+ */
 public class RunningProcessesReaderEvent extends AbstractPeriodicEvent implements ISensor {
 
-    private ActivityManager m_activityManager;
+    private ActivityManager mActivityManager;
     // private Query<SensorRunningProcesses> m_query;
-    private ArrayList<String> m_liLastProcesses = new ArrayList<String>();
+    private List<String> mLastProcesses = new ArrayList<>();
+
+    private String currentProcessName;
 
     public RunningProcessesReaderEvent(Context context) {
         super(context);
+
         setDataIntervalInSec(30);
-        m_activityManager = (ActivityManager) this.context.getSystemService(Context.ACTIVITY_SERVICE);
-        // m_query =
-        // mDaoSession.getSensorRunningProcessesDao().queryBuilder().where(Properties.RunningProcesses.eq("")).build();
+        mActivityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
     }
 
     @Override
     public void dumpData() {
 
+        DbRunningProcessesEvent runningProcessesEvent = new DbRunningProcessesEvent();
+
+        runningProcessesEvent.setRunningProcesses(currentProcessName);
+        runningProcessesEvent.setCreated(DateUtils.dateToISO8601String(new Date(), Locale.getDefault()));
+
+        daoProvider.getRunningProcessesEventDao().insert(runningProcessesEvent);
     }
 
     @Override
@@ -40,42 +54,36 @@ public class RunningProcessesReaderEvent extends AbstractPeriodicEvent implement
     @Override
     public void reset() {
 
+        currentProcessName = "";
     }
 
     @Override
     protected void getData() {
 
-        List<RunningAppProcessInfo> liProcesses = m_activityManager.getRunningAppProcesses();
+        List<RunningAppProcessInfo> liProcesses = mActivityManager.getRunningAppProcesses();
 
-        ArrayList<String> liProcessNames = new ArrayList<String>(liProcesses.size());
-        boolean bProcessesChanged = liProcesses.size() != m_liLastProcesses.size();
+        List<String> processNames = new ArrayList<>(liProcesses.size());
+        boolean bProcessesChanged = liProcesses.size() != mLastProcesses.size();
+
         for (RunningAppProcessInfo process : liProcesses) {
-            liProcessNames.add(process.processName);
-            if (!bProcessesChanged && !m_liLastProcesses.contains(process.processName))
+            processNames.add(process.processName);
+            if (!bProcessesChanged && !mLastProcesses.contains(process.processName))
                 bProcessesChanged = true;
         }
 
         if (bProcessesChanged) {
-            m_liLastProcesses = liProcessNames;
 
-            long longTimestamp = Calendar.getInstance().getTimeInMillis();
-            for (String processName : liProcessNames) {
+            mLastProcesses = processNames;
+
+            for (String processName : processNames) {
                 // m_query.setParameter(0, processName);
                 // List<SensorRunningProcesses> li = m_query.list();
                 // if (li == null || li.size() == 0)
                 // {
-//				SensorRunningProcesses sensor = new SensorRunningProcesses();
-//				sensor.setRunningProcesses(processName);
-//				sensor.setTimestamp(longTimestamp);
-//				handleDBEntry(sensor, false, false, true);
+                currentProcessName = processName;
+                dumpData();
                 // }
             }
         }
     }
-
-//	@Override
-//	public MessageType getMessageType() {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
 }
