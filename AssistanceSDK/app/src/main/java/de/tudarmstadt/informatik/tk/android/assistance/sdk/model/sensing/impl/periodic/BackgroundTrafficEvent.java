925194e137cfabc6f2eedeaffae6a489517d8ac1
Wladimir Schmidt <wlsc.dev@gmail.com>
2015-11-24 21:05:54 +0100

    * modified BackgroundTrafficEvent to new structure

--- AssistanceSDK/app/src/main/java/de/tudarmstadt/informatik/tk/android/assistance/sdk/model/sensing/impl/periodic/BackgroundTrafficEvent.java
+++ AssistanceSDK/app/src/main/java/de/tudarmstadt/informatik/tk/android/assistance/sdk/model/sensing/impl/periodic/BackgroundTrafficEvent.java
@@ -3,41 +3,53 @@ package de.tudarmstadt.informatik.tk.android.assistance.sdk.model.sensing.impl.p
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.net.TrafficStats;
 
 import java.util.List;
 
+import de.tudarmstadt.informatik.tk.android.assistance.sdk.db.DbNetworkTrafficEvent;
 import de.tudarmstadt.informatik.tk.android.assistance.sdk.model.api.dto.DtoType;
 import de.tudarmstadt.informatik.tk.android.assistance.sdk.model.enums.EPushType;
 import de.tudarmstadt.informatik.tk.android.assistance.sdk.model.sensing.AbstractPeriodicEvent;
 import de.tudarmstadt.informatik.tk.android.assistance.sdk.model.sensing.ISensor;
+import de.tudarmstadt.informatik.tk.android.assistance.sdk.model.sensing.impl.triggered.ForegroundTrafficEvent;
+import de.tudarmstadt.informatik.tk.android.assistance.sdk.provider.PreferenceProvider;
 
 /**
  * This is a Periodic Sensor class which collect the mobile traffic data produced by the apps in the
  * background in periodic intervals. This is important to get the traffic from apps which runs
  * services in background.
  * <p/>
- * Created by Stefan Hacker on 09.12.14.
+ * Created by Stefan Hacker on 09.12.14
+ *
+ * @edited by Wladimir Schmidt (wlsc.dev@gmail.com)
+ * @date 24.11.2015
  */
 public class BackgroundTrafficEvent extends
         AbstractPeriodicEvent implements
         ISensor {
 
     private static final int INIT_DATA_INTERVAL = 5 * 60;
-    private PackageManager m_PackageManager;
+    private PackageManager packageManager;
 
     /**
      * Constructor of a new Background Traffic Sensor
      *
-     * @param context global information about Kraken.Me app
+     * @param context global information about assistance app
      */
     public BackgroundTrafficEvent(Context context) {
         super(context);
+
         setDataIntervallInSec(INIT_DATA_INTERVAL);
-        m_PackageManager = this.context.getPackageManager();
+        packageManager = context.getPackageManager();
+
         //initial Data
-//        SensorNetworkTraffic sensorNetworkTraffic = new SensorNetworkTraffic();
-//        sensorNetworkTraffic.setAppName(ForegroundTrafficSensor.EVENT_START_KRAKEN);
-//        handleDBEntry(sensorNetworkTraffic);
+        DbNetworkTrafficEvent networkTrafficEvent = new DbNetworkTrafficEvent();
+        networkTrafficEvent.setAppName(ForegroundTrafficEvent.EVENT_START_ASSISTANCE);
+        networkTrafficEvent.setBackground(true);
+
+        daoProvider.getNetworkTrafficEventDao().insert(networkTrafficEvent);
+
         getData();
     }
 
@@ -47,8 +59,20 @@ public class BackgroundTrafficEvent extends
      */
     @Override
     protected void getData() {
-        List<ApplicationInfo> packages = m_PackageManager.getInstalledApplications(PackageManager.GET_META_DATA);
+
+        List<ApplicationInfo> packages = packageManager
+                .getInstalledApplications(PackageManager.GET_META_DATA);
+
+        if (packages == null) {
+            return;
+        }
+
         for (ApplicationInfo packageInfo : packages) {
+
+            if (packageInfo == null) {
+                continue;
+            }
+
             insertData(packageInfo);
         }
     }
@@ -59,16 +83,25 @@ public class BackgroundTrafficEvent extends
      * @param packageInfo app which will insert in Database
      */
     private void insertData(ApplicationInfo packageInfo) {
-//        SensorNetworkTraffic sensorNetworkTraffic = new SensorNetworkTraffic();
-//        if (TrafficStats.getUidTxPackets(packageInfo.uid) != 0 || TrafficStats.getUidRxPackets(packageInfo.uid) != 0) {
-//            sensorNetworkTraffic.setAppName(packageInfo.packageName);
-//            sensorNetworkTraffic.setTxBytes(TrafficStats.getUidTxPackets(packageInfo.uid));
-//            sensorNetworkTraffic.setRxBytes(TrafficStats.getUidRxPackets(packageInfo.uid));
-//            sensorNetworkTraffic.setBackground(true);
-//            sensorNetworkTraffic.setLatitude(TrafficLocation.getInstance().getLatitude());
-//            sensorNetworkTraffic.setLongitude(TrafficLocation.getInstance().getLongitude());
-//            handleDBEntry(sensorNetworkTraffic);
-//        }
+
+        DbNetworkTrafficEvent networkTrafficEvent = new DbNetworkTrafficEvent();
+
+        if (TrafficStats.getUidTxPackets(packageInfo.uid) != 0 ||
+                TrafficStats.getUidRxPackets(packageInfo.uid) != 0) {
+
+            networkTrafficEvent.setAppName(packageInfo.packageName);
+            networkTrafficEvent.setTxBytes(TrafficStats.getUidTxPackets(packageInfo.uid));
+            networkTrafficEvent.setRxBytes(TrafficStats.getUidRxPackets(packageInfo.uid));
+            networkTrafficEvent.setBackground(true);
+
+            Double lastLatitude = PreferenceProvider.getInstance(context).getLastLatitude();
+            Double lastLongitude = PreferenceProvider.getInstance(context).getLastLongitude();
+
+            networkTrafficEvent.setLatitude(lastLatitude);
+            networkTrafficEvent.setLongitude(lastLongitude);
+
+            daoProvider.getNetworkTrafficEventDao().insert(networkTrafficEvent);
+        }
     }
 
     /**
