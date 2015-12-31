3e2d339f44e62269a69f8166360af3cc8fcd252f
Wladimir Schmidt <wlsc.dev@gmail.com>
2015-12-31 17:51:18 +0100

    + added SocialProvider, FacebookProvider and handler for user graph data request

---
+++ AssistanceSDK/app/src/main/java/de/tudarmstadt/informatik/tk/android/assistance/sdk/provider/SocialProvider.java
@@ -0,0 +1,42 @@
+package de.tudarmstadt.informatik.tk.android.assistance.sdk.provider;
+
+import android.content.Context;
+
+import de.tudarmstadt.informatik.tk.android.assistance.sdk.provider.social.FacebookProvider;
+
+/**
+ * @author Wladimir Schmidt (wlsc.dev@gmail.com)
+ * @date 31.12.2015
+ */
+public class SocialProvider {
+
+    private static final String TAG = SocialProvider.class.getSimpleName();
+
+    private static SocialProvider INSTANCE;
+
+    private static Context mContext;
+
+    private SocialProvider(Context context) {
+        this.mContext = context;
+    }
+
+    public static SocialProvider getInstance(Context context) {
+
+        if (INSTANCE == null) {
+            INSTANCE = new SocialProvider(context);
+        }
+
+        mContext = context;
+
+        return INSTANCE;
+    }
+
+    /**
+     * Provides implementation for Facebook provider
+     *
+     * @return
+     */
+    public static FacebookProvider getFacebookProvider() {
+        return FacebookProvider.getInstance(mContext);
+    }
+}
