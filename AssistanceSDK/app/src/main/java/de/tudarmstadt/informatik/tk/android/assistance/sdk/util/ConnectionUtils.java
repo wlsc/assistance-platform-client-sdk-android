216db36dbc5f150fe6ed855ac8cd550fd2a11c7b
Wladimir Schmidt <wlsc.dev@gmail.com>
2016-01-05 18:54:14 +0100

    + added function to get current network connection type

--- AssistanceSDK/app/src/main/java/de/tudarmstadt/informatik/tk/android/assistance/sdk/util/ConnectionUtils.java
+++ AssistanceSDK/app/src/main/java/de/tudarmstadt/informatik/tk/android/assistance/sdk/util/ConnectionUtils.java
@@ -5,6 +5,7 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Build;
 import android.provider.Settings;
+import android.telephony.TelephonyManager;
 
 /**
  * @author Wladimir Schmidt (wlsc.dev@gmail.com)
@@ -120,5 +121,68 @@ public class ConnectionUtils {
 
         ConnectivityManager manager = getConnectivityManager(context);
         return manager != null && manager.isActiveNetworkMetered();
+    }
+
+    /**
+     * Gives current network connection type
+     *
+     * @param context
+     * @return
+     */
+    public static int getConnectionType(Context context) {
+
+        int result = -1;
+        NetworkInfo networkInfo = getActiveNetwork(context);
+        TelephonyManager telephonyManager =
+                (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+
+        if (networkInfo == null) {
+            return -1;
+        }
+
+        int networkType = networkInfo.getType();
+
+        switch (networkType) {
+            case ConnectivityManager.TYPE_WIFI:
+                result = ConnectivityManager.TYPE_WIFI;
+                break;
+            case ConnectivityManager.TYPE_WIMAX:
+                result = ConnectivityManager.TYPE_WIMAX;
+                break;
+            case ConnectivityManager.TYPE_MOBILE:
+                switch (telephonyManager.getNetworkType()) {
+                    case TelephonyManager.NETWORK_TYPE_LTE:
+                        result = TelephonyManager.NETWORK_TYPE_LTE;
+                        break;
+                    case TelephonyManager.NETWORK_TYPE_HSPA:
+                        result = TelephonyManager.NETWORK_TYPE_HSPA;
+                        break;
+                    case TelephonyManager.NETWORK_TYPE_HSPAP:
+                        result = TelephonyManager.NETWORK_TYPE_HSPAP;
+                        break;
+                    case TelephonyManager.NETWORK_TYPE_HSDPA:
+                        result = TelephonyManager.NETWORK_TYPE_HSDPA;
+                        break;
+                    case TelephonyManager.NETWORK_TYPE_EDGE:
+                        result = TelephonyManager.NETWORK_TYPE_EDGE;
+                        break;
+                    case TelephonyManager.NETWORK_TYPE_GPRS:
+                        result = TelephonyManager.NETWORK_TYPE_GPRS;
+                        break;
+                    case TelephonyManager.NETWORK_TYPE_CDMA:
+                        result = TelephonyManager.NETWORK_TYPE_CDMA;
+                        break;
+                    case TelephonyManager.NETWORK_TYPE_UMTS:
+                        result = TelephonyManager.NETWORK_TYPE_UMTS;
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            default:
+                break;
+        }
+
+        return result;
     }
 }
