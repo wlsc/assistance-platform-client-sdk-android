08b3dd37a502813f92ff2008d7a0dbf99e00f480
Wladimir Schmidt <wlsc.dev@gmail.com>
2016-02-01 11:46:55 +0100

    [BatteryWatcher]: + added DB schemas generator + added DAO provider and db open helper, etc + added BatteryUtils * generated first schemas

---
+++ Codegen/src/de/tudarmstadt/informatik/tk/android/assistance/generators/BatteryWatcherDatabaseGenerator.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package de.tudarmstadt.informatik.tk.android.assistance.generators;
+
+import java.io.File;
+import java.io.IOException;
+
+import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Schema;
+import de.tudarmstadt.informatik.tk.android.assistance.Config;
+
+/**
+ * @author Wladimir Schmidt
+ * @date 01.02.2016
+ *
+ */
+public class BatteryWatcherDatabaseGenerator {
+  
+  public static void main(String[] args) throws Exception {
+    new File(Config.BATTERY_WATCHER_OUTPUT).mkdirs();
+    generateSchemas();
+  }
+    
+  private static void generateSchemas() throws Exception, IOException {
+    
+    Schema schema = new Schema(Config.BATTERY_WATCHER_DB_SCHEMA_VERSION, Config.BATTERY_WATCHER_PACKAGE);
+     
+    // ----- Measurements -----
+    Entity measurements = schema.addEntity("Measurement");
+    measurements.setTableName("measurement");
+    measurements.addIdProperty().autoincrement().index();
+    measurements.addIntProperty("voltage");
+    measurements.addIntProperty("cpu");
+    measurements.addIntProperty("memory");
+    measurements.addStringProperty("created").notNull();
+    
+    // **************************
+    // **** GENERATE DAOs ****
+    // **************************
+    new DaoGenerator().generateAll(schema, Config.BATTERY_WATCHER_OUTPUT);
+  }
+}
